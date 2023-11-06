1b85fd4c760b06f65a25df82c07243db54229ec8
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-11-06 16:39:27 -0400

    conectando con el servidor

--- Server/routes/iniciar_sesion.js
+++ Server/routes/iniciar_sesion.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  console.log("llegando señales");
+  res.send( req.body );
+
 });
 
 
